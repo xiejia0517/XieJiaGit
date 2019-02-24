92522785720242491fa91721d7c091f3d7748b27
xiejia <278772051@qq.com>
2019-02-24 17:03:04 +0800

    漏提交文件

---
+++ public/static/admin/js/test.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $(".changestatus").click(function (e) { 
+        
+        layer.msg('请先上传图片111111');
+    });
+});
---
+++ public/static/admin/style/test.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    new Vue({
+        el:"#box",
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            test:function(){
+                alert(55);
+            },
+        },
+    });
+});
